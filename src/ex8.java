eefc5878222522e036ee06594444a00f66067043
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 20:03:20 -0300

    Atualizando Ex 6

--- src/ex8.java
+++ src/ex8.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class ex8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num, dezena;
+        int num;
         System.out.println("Digite um numero de 3 digitos: ");
         num = sc.nextInt();
-        dezena = (num /10) % 10;
-        System.out.println("O valor da dezena é: " + dezena);
+        num = num /10 % 10;
+        System.out.println("O valor da dezena é: " + num);
 
 
     }
